6ec482e252afdf975d3f0cd2f85f7c568c56c8ad
Yeşim Bozkurt <yesimsbozkurt@gmail.com>
2025-02-04 15:41:22 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+const profile = {
+	username: "Jacob",
+  playTime: 300,
+  getInfo: function() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  changeUsername: function(newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function(hours) {
+    this.playTime += hours;
+  }
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+//
